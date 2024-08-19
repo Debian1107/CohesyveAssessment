b83fe73e07fb2cf5453014316dc098f03f53d810
lilnxxx <swastik7210@gmail.com>
2024-08-19 20:19:47 +0530

    added competitors and explore competitiors in frontend + List of investors and founders

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -38,6 +38,11 @@ export default function Dashboard() {
       {companyName && is_searching ? (
         <div className="">
           <CompanyProfile companyName={companyName} />
+          <InvestorCofounder companyName={companyName} />
+          <Competitors
+            companyName={companyName}
+            setCompanyname={setCompanyname}
+          />
         </div>
       ) : (
         <></>
@@ -47,17 +52,6 @@ export default function Dashboard() {
 }
 
 const CompanyProfile = ({ companyName }) => {
-  const company2 = {
-    name: "Tech Innovations Ltd.",
-    industry: "Software Development",
-    location: "San Francisco, CA",
-    founded: "2010",
-    employees: "200+",
-    revenue: "$50M+",
-    description:
-      "Tech Innovations Ltd. is a leading software development company specializing in AI-driven solutions and cloud services.",
-    website: "https://techinnovations.com",
-  };
   const [company, setCompany] = useState(companyName);
   useEffect(() => {
     const handelCompanysearch = async () => {
@@ -86,13 +80,13 @@ const CompanyProfile = ({ companyName }) => {
     handelCompanysearch();
   }, [companyName]);
   return (
-    <div className="bg-gray-100 min-h-screen p-8 flex justify-center items-start">
+    <div className="bg-gray-100  pt-8 flex justify-center items-start">
       <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-4xl">
         <h2 className="text-2xl font-bold mb-4 text-gray-800 border-b pb-2">
           {company.name}
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
           <div>
             <p className="text-gray-600">
               <span className="font-semibold">usesFacebookAds: </span>
@@ -208,6 +202,159 @@ const CompanyProfile = ({ companyName }) => {
                 Visit Website
               </a>
             </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const InvestorCofounder = ({ companyName }) => {
+  const [investors, setInvestors] = useState([]);
+  const [cofounders, setCofounders] = useState([]);
+  useEffect(() => {
+    const handelCompanyinvestor = async () => {
+      console.log("company investor using query is -- ", companyName);
+      if (!companyName) return;
+      try {
+        const response = await fetch(
+          "http://localhost:8000/data/all-investor",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ query: companyName }),
+          }
+        );
+        if (!response.ok) {
+          console.log("response not ok investor");
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        console.log("comapny investor data is ", result);
+        setInvestors(result.data);
+      } catch (error) {
+        console.log("error during searching company by query");
+      }
+    };
+    const handelCompanyfounders = async () => {
+      console.log("company search using query is -- ");
+      if (!companyName) return;
+      try {
+        const response = await fetch(
+          "http://localhost:8000/data/all-cofounder",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ query: companyName }),
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        console.log("company cofounder data is ", result);
+        setCofounders(result.data);
+      } catch (error) {
+        console.log("error during searching company by query");
+      }
+    };
+    handelCompanyinvestor();
+    handelCompanyfounders();
+  }, [companyName]);
+
+  return (
+    <div className="bg-gray-100  pt-8 flex justify-center items-start">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-4xl">
+        <h2 className="text-2xl flex gap-44 font-bold mb-4 text-gray-800 border-b pb-2 px-1">
+          <p>Institutional Investors</p>
+          <p>Cofounders</p>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-9 max-h-[50vh] overflow-y-auto">
+          <div>
+            {investors.length
+              ? investors.map((ind, i) => {
+                  return <p id={i}>{ind.name}</p>;
+                })
+              : null}
+          </div>
+          <div>
+            {cofounders.length
+              ? cofounders.map((ind, i) => {
+                  return <p id={i}>{ind.name}</p>;
+                })
+              : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Competitors = ({ companyName, setCompanyname }) => {
+  const [competitors, setCompetitors] = useState([]);
+  useEffect(() => {
+    const handelCompanycompetitors = async () => {
+      console.log("company investor using query is -- ", companyName);
+      if (!companyName) return;
+      try {
+        const response = await fetch(
+          "http://localhost:8000/data/all-competitors",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ query: companyName }),
+          }
+        );
+        if (!response.ok) {
+          console.log("response not ok investor");
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        console.log("Competitors of company are ", result);
+        setCompetitors(result.data);
+      } catch (error) {
+        console.log("error during searching company by query");
+      }
+    };
+
+    handelCompanycompetitors();
+  }, [companyName]);
+
+  return (
+    <div className="bg-gray-100 min-h-screen pt-8 flex justify-center items-start">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-4xl">
+        <h2 className="text-2xl flex  font-bold mb-4 text-gray-800 border-b pb-2 px-1">
+          <p>Competitors</p>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-9 max-h-[50vh] overflow-y-auto">
+          <div>
+            {competitors.length
+              ? competitors.map((ind, i) => {
+                  return (
+                    <p
+                      key={i}
+                      onClick={() => {
+                        setCompanyname(ind.name);
+                        window.scrollTo({
+                          top: 0,
+                          behavior: "smooth",
+                        });
+                      }}
+                      className="border-[2px] text-blue-900 px-4 py-3 rounded-full shadow-md hover:shadow-lg hover:bg-blue-200 transition-all duration-300 cursor-pointer text-center w-full"
+                    >
+                      {ind.name}
+                    </p>
+                  );
+                })
+              : null}
           </div>
         </div>
       </div>
